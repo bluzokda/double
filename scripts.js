18858ddb192d3e4ce4b4e487cde8865b25f3339e
bluzokda <babuk.123@mail.ru>
2025-05-22 21:06:04 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', async function () {
     loadUserProgress(session.user.id);
   }
 });
-const supabaseUrl = 'https://tekxmqrbpdzmbcjszksg.supabase.co ';
+const supabaseUrl = 'https://tekxmqrbpdzmbcjszksg.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRla3htcXJicGR6bWJjanN6a3NnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczMzE4NTAsImV4cCI6MjA2MjkwNzg1MH0.YLJrqLBam99cYu0_ZTi-I57kYw7aCrilHyriTwLVYZ4';
 const supabase = createClient(supabaseUrl, supabaseKey);
 // Глобальные переменные
